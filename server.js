d28f2be49bf8b993e3ee76d13b5483bc411bc5ab
Alvaro Junqueira <alvarocjunq@gmail.com>
2018-10-18 16:10:47 +0100

    cors

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const cors = require('cors');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/build'));
-app.use(cors())
-
-app.get('/*', function(req,res) {
+app.use(express.static(__dirname + '/dist'));
+app.use(cors({ origin: '*' }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+app.get('/*', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.sendFile(path.join(__dirname+'/build/index.html'));
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', '*');
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
